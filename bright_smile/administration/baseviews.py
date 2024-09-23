9a80c81faf9db2ec6c24be33a758204c6b6fdc7b
manavumd <manavg@umd.edu>
2024-09-23 15:57:21 -0400

    added api view set for specialties

--- bright_smile/administration/baseviews.py
+++ bright_smile/administration/baseviews.py
@@ -5,6 +5,6 @@ from .serializers import SpecialtySerializer
 
 
 class SpecialityViewSet(viewsets.ModelViewSet):
-    queryset = Specialty.objects.none()
+    queryset = Specialty.objects.all()
     serializer_class = SpecialtySerializer
 
