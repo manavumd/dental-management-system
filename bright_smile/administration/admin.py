d98fa9a7d0adb780937449561d3de8feb6c17942
manavumd <manavg@umd.edu>
2024-09-24 19:39:04 -0400

    registered speciality to admin

--- bright_smile/administration/admin.py
+++ bright_smile/administration/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Clinic, Doctor, Patient
+from .models import Clinic, Doctor, Patient, Specialty
 
 admin.site.register(Clinic)
 admin.site.register(Doctor)
 admin.site.register(Patient)
+admin.site.register(Specialty)
